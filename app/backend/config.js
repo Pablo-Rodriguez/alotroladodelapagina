703ba220e47853ed82506c93d6501ae574e56787
Pablo-Rodriguez <pablorp1996@gmail.com>
2016-10-30 12:19:56 +0100

    Posibilidad de conectarse a la base de datos de produccion desde desarrollo

--- app/backend/config.js
+++ app/backend/config.js
@@ -1,4 +1,30 @@
 
+import inquirer from 'inquirer';
+
 export default {
-    port: 5000
+    port: 5000,
+    db: function (env) {
+        return new Promise((resolve, reject) => {
+            if (env === 'pro') {
+                if (process.env.MONGODB_URI) {
+                    resolve(process.env.MONGODB_URI);
+                } else {
+                    inquirer.prompt([{
+                        name: 'user',
+                        message: 'username?',
+                        default: ''
+                    },{
+                        type: 'password',
+                        name: 'pass',
+                        message: 'password?',
+                        default: ''
+                    }]).then((answers) => {
+                        resolve(`mongodb://${answers.user}:${answers.pass}@ds139197.mlab.com:39197/heroku_s06kk7fb`);
+                    });
+                }
+            } else {
+                resolve('mongodb://localhost/alotroladodelapagina');
+            }
+        });
+    }
 }
--- app/backend/server.js
+++ app/backend/server.js
@@ -13,18 +13,21 @@ const app = new Rex(router);
 const server = http.createServer(router);
 
 const port = process.env.PORT || config.port;
-const db = process.env.MONGODB_URI || 'mongodb://localhost/alotroladodelapagina';
 
 app.setMiddlewares(middlewares);
 app.setControllers(controllers);
 
 mongoose.Promise = global.Promise;
-mongoose.connect(db, function (err) {
-	if(err){
-		console.log("ERROR: " + err);
-	}else{
-		console.log("Conexion a BD con exito");
-	}
-});
+
+config.db(process.env.ENV_CHOICE)
+	.then((db) => {
+		mongoose.connect(db, function (err) {
+			if(err){
+				console.log("ERROR: " + err);
+			}else{
+				console.log("Conexion a BD con exito");
+			}
+		});
+	});
 
 server.listen(port, () => console.log('todo correcto'));
